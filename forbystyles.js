59d2548c1b66cfaa1f19c140bf3acf3a163c3ac5
Edarth002 <138538691+Edarth002@users.noreply.github.com>
2023-07-23 06:00:08 -0700

    Build: Upgraded to new file structure

---
+++ forbystyles.js
@@ -0,0 +1,10 @@
+
+
+var navlinks = document.getElementById("navlinks");
+function showMenu() {
+    navlinks.style.display = "block";
+}
+
+function hideMenu() {
+    navlinks.style.display = "none";
+}
